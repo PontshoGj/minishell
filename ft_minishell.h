2bc943e572c95f9c744201729d3980acf3f62298
Pontsho MOGWERE <pmogwere@c5r6s3.wethinkcode.co.za>
2020-01-30 17:25:22 +0200

    fix the errors

--- ft_minishell.h
+++ ft_minishell.h
@@ -7,7 +7,7 @@ extern char **environ;
 char    *ft_pwd();
 void    ft_cd(char *s);
 void    ft_envir();
-void    ft_setenv();
+void    ft_setenv(char *new_var);
 void    ft_unsetenv(char *var);
 int     ft_strarrlen(char **arr);
 void    ft_echo(char *input);
